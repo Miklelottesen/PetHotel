eafdfb70e12bdaca51ef15be4d04ec8ff6ddd1c5
Melectrian <dev.melec@gmail.com>
2016-03-16 10:49:48 +0100

    person

--- PetHotel/Models/Person.cs
+++ PetHotel/Models/Person.cs
@@ -7,9 +7,9 @@ namespace PetHotel.Models
 {
     public class Person
     {
-        public int PersonID { get; set; };
-        public string Firstname { get; set; };
-        public string Lastname { get; set; };
-        public string Phone { get; set; };
+        public int PersonID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
     }
 }
