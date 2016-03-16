cd3ae068661be4ece3e0ded6182868552b0dd9e0
Melectrian <dev.melec@gmail.com>
2016-03-16 10:46:40 +0100

    Reservation class

--- PetHotel/Models/Reservation.cs
+++ PetHotel/Models/Reservation.cs
@@ -7,5 +7,55 @@ namespace PetHotel.Models
 {
     public class Reservation
     {
+        public int ReservationID
+        {
+            get;
+        }
+
+        public String PetName
+        {
+            get; set;
+        }
+
+        public DateTime Birthday
+        {
+            get; set;
+        }
+
+        public String Specie
+        {
+            get; set;
+        }
+
+        public DateTime StartDate
+        {
+            get; set;
+        }
+
+        public DateTime EndDate
+        {
+            get; set;
+        }
+
+        public Customer Customer
+        {
+            get;
+        }
+
+        public String Employee
+        {
+            get; set;
+        }
+
+        public Reservation(int ReservationID, String PetName, String Specie, DateTime StartDate, DateTime EndDate, Customer Customer)
+        {
+            this.ReservationID = ReservationID;
+            this.PetName = PetName;
+            this.Specie = Specie;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+            this.Customer = Customer;
+        }
+
     }
 }
