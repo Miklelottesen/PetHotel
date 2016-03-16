ba06f82cf4eade3c73f5a2560b08d73c4701e44d
Miklelottesen <miklelottesen@gmail.com>
2016-03-16 11:09:23 +0100

    Fixed syntax errors

--- PetHotel/Models/Employee.cs
+++ PetHotel/Models/Employee.cs
@@ -7,8 +7,8 @@ namespace PetHotel.Models
 {
     public class Employee : Person
     {
-        public string Initials { get; set; };
-        public List<Reservation> Reservations { get; set; };
+        public string Initials { get; set; }
+        public List<Reservation> Reservations { get; set; }
 
         public Employee (int PersonID, string Firstname, string Lastname, string Initials, string Phone)
         {
