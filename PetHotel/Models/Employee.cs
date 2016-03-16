5e624403173220e8cb24a92cda814014909f9673
Miklelottesen <miklelottesen@gmail.com>
2016-03-16 11:07:23 +0100

    Populated Employee

--- PetHotel/Models/Employee.cs
+++ PetHotel/Models/Employee.cs
@@ -5,9 +5,18 @@ using System.Web;
 
 namespace PetHotel.Models
 {
-    public class Employee
+    public class Employee : Person
     {
         public string Initials { get; set; };
         public List<Reservation> Reservations { get; set; };
+
+        public Employee (int PersonID, string Firstname, string Lastname, string Initials, string Phone)
+        {
+            this.PersonID = PersonID;
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Initials = Initials;
+            this.Phone = Phone;
+        }
     }
 }
