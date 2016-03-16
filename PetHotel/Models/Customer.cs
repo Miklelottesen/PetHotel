88a4a2a2353d5010277b5cee95c9170538e17484
Miklelottesen <miklelottesen@gmail.com>
2016-03-16 10:27:44 +0100

    Populated Customer class

---
+++ PetHotel/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetHotel.Models
+{
+    public class Customer
+    {
+        public string Address
+        {
+            get; set;
+        };
+        public string Zipcode
+        {
+            get; set;
+        };
+        public string City
+        {
+            get; set;
+        };
+        public string Email
+        {
+            get; set;
+        };
+    }
+}
